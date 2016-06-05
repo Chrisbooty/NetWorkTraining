ebd63f5ce76ac2333275678d7f5f2b1ef5fd3e33
Chrisbooty <1163209271@qq.com>
2016-06-05 09:57:48 +0800

    Initial Commit

---
+++ NetworkTraining/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetworkTraining
+//
+//  Created by mac on 16/6/5.
+//  Copyright © 2016年 Cijian.Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NetworkTraining/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NetworkTraining
+//
+//  Created by mac on 16/6/5.
+//  Copyright © 2016年 Cijian.Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
